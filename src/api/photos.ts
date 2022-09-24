4ab0027461e77cf9e903422f7648ae30e8e5a1af
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-09-24 19:06:39 +0300

    Code clearing

--- src/api/photos.ts
+++ src/api/photos.ts
@@ -2,4 +2,3 @@ import { Photo } from '../types/Photo';
 import { client } from '../utils/fetchClient';
 
 export const getPhotos = () => client.get<Photo[]>('');
-// export const getUser = (userId: number) => get<User>(`/users/${userId}`);
